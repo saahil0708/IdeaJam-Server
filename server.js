6f6b98713ef29f3d6e37636a1c86d89fa186f681
Saahil <saahil.ind.dgp@gmail.com>
2025-08-11 15:05:32 +0530

    Changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: ['http://localhost:3000', 'https://ideajam.vercel.app'],
+        origin: [ 'https://ideajam.vercel.app', 'http://localhost:3000',],
     }
 ));
 
