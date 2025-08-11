d69298730a4484fbb155a3a6d7709892ebf83a58
Saahil <saahil.ind.dgp@gmail.com>
2025-08-11 14:44:52 +0530

    URL Added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://ideajam.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
         allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
         credentials: true,
